3da6952d27ccd5cbb85dfef059250bcab5930501
wlj <849027724@qq.com>
2020-09-08 15:33:35 +0800

    fix

---
+++ src/main/java/com/time/manager/TimeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.time.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManagerApplication.class, args);
+    }
+
+}
