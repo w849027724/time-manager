d08d1c9b1f57ed3522699a707993e68f78c260a5
wlj <wulijun199556>
2020-09-09 22:13:34 +0800

    fix

--- time-manager-cms/src/main/java/com/time/manager/cms/controller/UserController.java
+++ time-manager-cms/src/main/java/com/time/manager/cms/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.time.manager.cms.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.time.manage.common.core.utils.R;
+import com.time.manager.cms.entity.UserInfo;
+import com.time.manager.cms.service.UserInfoService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author wlj
@@ -11,12 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @Api(tags = "用户")
+@RequiredArgsConstructor
 public class UserController {
-
+    private final UserInfoService userInfoService;
 
     @GetMapping("/info")
     public String getUserInfo() {
         return "左丽牛逼！！";
+    }
+
+    @PostMapping("/login")
+    public R login(@RequestBody UserInfo userInfo) {
+        List<UserInfo> list = userInfoService.list(Wrappers.<UserInfo>query()
+                .lambda().eq(UserInfo::getUserName, userInfo.getUserName()));
+        if (list.size() > 0) {
+            UserInfo userInfo1 = list.get(0);
+            if (userInfo1.getUserPassword().equals(userInfo.getUserPassword())) {
+                return R.ok(userInfo1);
+            }
+        }
+        return R.failed();
     }
 
 }
