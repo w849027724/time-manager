9e714bbf164d24bf3a8590210376715ee1d56b8c
wlj <849027724@qq.com>
2020-09-14 17:24:08 +0800

    fix

--- time-manager-cms/src/main/java/com/time/manager/cms/controller/app/FindController.java
+++ time-manager-cms/src/main/java/com/time/manager/cms/controller/app/FindController.java
@@ -1,18 +1,22 @@
 package com.time.manager.cms.controller.app;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.time.manage.common.core.utils.R;
 import com.time.manager.cms.entity.PlanInfo;
 import com.time.manager.cms.entity.PlanStat;
 import com.time.manager.cms.service.PlanInfoService;
 import com.time.manager.cms.service.PlanStatService;
+import com.time.manager.cms.vo.PlanInfoVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,21 +33,27 @@ public class FindController {
 
     @GetMapping("/list")
     @ApiOperation("发现列表")
-    public R findList() {
+    public R<List<PlanInfoVO>> findList() {
         List<PlanInfo> list = planInfoService.list();
-        return R.ok(list);
+        List<PlanInfoVO> result = new ArrayList<>(list.size());
+        list.forEach(e -> {
+            PlanInfoVO planInfoVO = new PlanInfoVO();
+            BeanUtils.copyProperties(e, planInfoVO);
+            Long planId = e.getPlanId();
+            PlanStat one = planStatService.getOne(Wrappers.<PlanStat>query().lambda().eq(PlanStat::getPlanId, planId));
+            if (ObjectUtil.isNotNull(one)) {
+                BeanUtils.copyProperties(one, planInfoVO);
+            }
+            result.add(planInfoVO);
+        });
+        return R.ok(result);
     }
 
 
     @PutMapping("/fabulous")
     @ApiOperation("用户点赞")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "planId", value = "planId", required = true, dataType = "Long", paramType = "query")
-    })
-    public R findFabulous(
-            @RequestParam("planId") Long planId
-    ) {
-        List<PlanStat> list = planStatService.list(Wrappers.<PlanStat>query().lambda().eq(PlanStat::getPlanId, planId));
+    public R findFabulous(@RequestBody PlanInfo planInfo) {
+        List<PlanStat> list = planStatService.list(Wrappers.<PlanStat>query().lambda().eq(PlanStat::getPlanId, planInfo.getPlanId()));
         if (list.size() > 0) {
             PlanStat planStat = list.get(0);
             planStat.setPlanFabulous(planStat.getPlanFabulous() + 1);
@@ -54,15 +64,8 @@ public class FindController {
 
     @PutMapping("/join")
     @ApiOperation("用户参加")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "planId", value = "planId", required = true, dataType = "Long", paramType = "query"),
-            @ApiImplicitParam(name = "userId", value = "userId", required = true, dataType = "Long", paramType = "query")
-    })
-    public R findFabulous(
-            @RequestParam("planId") Long planId,
-            @RequestParam("userId") Long userId
-    ) {
-        List<PlanStat> list = planStatService.list(Wrappers.<PlanStat>query().lambda().eq(PlanStat::getPlanId, planId));
+    public R findJoins(@RequestBody PlanInfo planInfo) {
+        List<PlanStat> list = planStatService.list(Wrappers.<PlanStat>query().lambda().eq(PlanStat::getPlanId, planInfo.getPlanId()));
         if (list.size() > 0) {
             // 更新参加数
             PlanStat planStat = list.get(0);
@@ -70,8 +73,11 @@ public class FindController {
             planStatService.updateById(planStat);
 
             PlanInfo byId = planInfoService.getById(planStat.getPlanId());
-            byId.setPlanId(null).setUserId(userId).setPlanStatus(0);
+            byId.setPlanId(null).setUserId(planInfo.getUserId()).setPlanStatus(0);
             planInfoService.save(byId);
+
+            planStat.setPlanStatId(null).setPlanId(byId.getPlanId()).setPlanJoins(0).setPlanFabulous(0).setPlanJoinUser("");
+            planStatService.save(planStat);
         }
         return R.ok();
     }
---
+++ time-manager-cms/src/main/java/com/time/manager/cms/vo/PlanInfoVO.java
@@ -0,0 +1,57 @@
+package com.time.manager.cms.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author wlj
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value = "计划信息vo", description = "")
+public class PlanInfoVO implements Serializable {
+
+    @ApiModelProperty(value = "计划id")
+    private Long planId;
+
+    @ApiModelProperty(value = "计划状态 1：未开始  2：进行中 3：未完成 4：已完成")
+    private Integer planStatus;
+
+    @ApiModelProperty(value = "用户id")
+    private Long userId;
+
+    @ApiModelProperty(value = "计划名字")
+    private String planName;
+
+    @ApiModelProperty(value = "计划类型 1：一次  2.每日 3.工作日 4.非工作日")
+    private Integer planType;
+
+    @ApiModelProperty(value = "计划开始时间")
+    private LocalDateTime planStartTime;
+
+    @ApiModelProperty(value = "计划结束时间")
+    private LocalDateTime planEndTime;
+
+    @ApiModelProperty(value = "计划秒数")
+    private Long planSecond;
+
+    @ApiModelProperty(value = "计划统计id")
+    private Long planStatId;
+
+    @ApiModelProperty(value = "计划点赞")
+    private Integer planFabulous;
+
+    @ApiModelProperty(value = "计划参与数")
+    private Integer planJoins;
+
+    @ApiModelProperty(value = "计划参与用户（json格式   user_id集合 英文逗号隔开）")
+    private String planJoinUser;
+
+}
