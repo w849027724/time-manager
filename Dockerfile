d22a3303ae9ba49cb450547ab39a949e631f2d46
wlj <849027724@qq.com>
2020-09-24 14:11:27 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM adoptopenjdk/openjdk8:jre8u265-b01
 COPY /time-manager-cms/target/*.jar time-manager-cms.jar
 EXPOSE 33001
-ENTRYPOINT ["java","-jar", "-Duser.timezone=GMT+8","-Djava.awt.headless=true","time-manager-cms.jar"]
+ENTRYPOINT ["java","-jar", "-Duser.timezone=GMT+8","-Djava.awt.headless=true --spring.profiles.active=test","time-manager-cms.jar"]
---
+++ date/time_manager.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 124.70.40.151
+ Source Server Type    : MySQL
+ Source Server Version : 50730
+ Source Host           : 124.70.40.151:3306
+ Source Schema         : time_manager
+
+ Target Server Type    : MySQL
+ Target Server Version : 50730
+ File Encoding         : 65001
+
+ Date: 23/09/2020 18:31:35
+*/
+
