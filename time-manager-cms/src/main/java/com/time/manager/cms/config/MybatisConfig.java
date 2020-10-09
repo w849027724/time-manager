d62b85f090564db20625e1868e1b0073148db72d
wlj <849027724@qq.com>
2020-10-09 14:50:22 +0800

    fix

--- time-manager-cms/src/main/java/com/time/manager/cms/config/MybatisConfig.java
+++ time-manager-cms/src/main/java/com/time/manager/cms/config/MybatisConfig.java
@@ -3,6 +3,9 @@ package com.time.manager.cms.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author wlj
+ */
 @Configuration
 @MapperScan("com.time.manager.cms.mapper")
 public class MybatisConfig {
