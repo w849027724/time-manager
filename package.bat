955009bde578b49595ba59c54eac8ac1ca149b24
wlj <849027724@qq.com>
2020-12-11 15:33:42 +0800

    fix

--- package.bat
+++ package.bat
@@ -9,21 +9,17 @@ set IMAGE_NAME_AUTH=time-manager-auth
 set IMAGE_VERSION=1.0.0
 set HARBOR_SERVER=registry.cn-hangzhou.aliyuncs.com
 
-call mvn clean install package
-
-
-
 call docker login --username=18250792991 --password=wlj@123456 registry.cn-hangzhou.aliyuncs.com
 
-call docker build -f /time-manager-service/time-manager-cms/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_CMS%:%IMAGE_VERSION% .
+call docker build -f time-manager-service/time-manager-cms/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_CMS%:%IMAGE_VERSION% .
 echo 开始推送cms...
 call docker push %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_CMS%:%IMAGE_VERSION%
 
-call docker build -f /time-manager-gateway/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_GATEWAY%:%IMAGE_VERSION% .
+call docker build -f time-manager-gateway/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_GATEWAY%:%IMAGE_VERSION% .
 echo 开始推送gateway...
 call docker push %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_GATEWAY%:%IMAGE_VERSION%
 
-call docker build -f /time-manager-auth/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_AUTH%:%IMAGE_VERSION% .
+call docker build -f time-manager-auth/Dockerfile -t %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_AUTH%:%IMAGE_VERSION% .
 echo 开始推送auth...
 call docker push %HARBOR_SERVER%/%PROJECT_NAME%/%IMAGE_NAME_AUTH%:%IMAGE_VERSION%
 
